780cc65445479a14d66bd33642aad0e435518ee8
Joel Guedes <153196370+JoelG-007@users.noreply.github.com>
2025-04-24 09:29:06 +0530

    Update A2.c

--- SY/Sem 4/Assignment 4 - Application of Graph/A2.c
+++ SY/Sem 4/Assignment 4 - Application of Graph/A2.c
@@ -1,74 +1,87 @@
 /*Dijkstra.*/
 
-#include<stdio.h>
+#include <stdio.h>
+
 #define INF -1
-#define MAX 20
+#define MAX 10
 
-void dijkstra(int n, int cost[MAX][MAX], int start){
-    int dist[MAX], visited[MAX] = {0};
-    int count = 1, min, next, i, j;
-
-    for(i = 0; i < n; i++)
-        dist[i] = cost[start][i];
-
-    visited[start] = 1;
-    dist[start] = 0;
-
-    while(count < n){
-        min = INF;
-        next = -1;
-
-        // Find the nearest unvisited vertex
-        for(i = 0; i < n; i++)
-            if(!visited[i] && dist[i] < min){
-                min = dist[i];
-                next = i;
-            }
-
-        if(next == -1) break; // No more reachable vertices
-
-        visited[next] = 1;
-        count++;
-
-        for(i = 0; i < n; i++)
-            if(!visited[i] && cost[next][i] != INF && dist[next] + cost[next][i] > dist[i])
-                dist[i] = dist[next] + cost[next][i];
-    }
-
-    printf("\nShortest distances from vertex %d:\n", start + 1);
-    for(i = 0; i < n; i++){
-        if(dist[i] == INF)
-            printf("To vertex %d: Unreachable\n", i + 1);
-        else
-            printf("To vertex %d: %d\n", i + 1, dist[i]);
-    }
-}
-
-int main(){
-    int n, cost[MAX][MAX], start, i, j;
-
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-
+void createmat(int cost[MAX][MAX], int n){
+    int i, j;
     printf("\nEnter the adjacency matrix (use %d for no direct path):\n", INF);
-    for(i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            if(i == j)
-                cost[i][j] = 0; // No cost for self-loops
-            else{
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            if(i == j) {
+                cost[i][j] = 0;
+            }else{
                 printf("Cost from vertex %d to vertex %d: ", i + 1, j + 1);
                 scanf("%d", &cost[i][j]);
             }
         }
     }
-    printf("\nEnter source Vertex - ");
-    scanf("%d",&start);
-    dijkstra(n, cost, start-1);
-    /*
-    for(i=0;i<n;i++){
-      printf("Source - v%d\n",i+1);
-      dijkstra(n, cost, i); //number of vertex, matrix, starting vertex
+}
+
+void dijkstra(int cost[MAX][MAX], int start, int n){
+    int dist[MAX], visited[MAX] = {0};
+    int i, u = 0, w, count, min;
+
+    for(i = 0; i < n; i++){
+        dist[i] = cost[start][i];
     }
-    */
+
+    dist[start] = 0;
+    visited[start] = 1;
+    count = 1;
+
+    while(count < n){
+        min = 999999; // Using a large number for comparison
+        u = -1;
+
+        // Find the unvisited vertex with the smallest known distance
+        for(i = 0; i < n; i++){
+            if (!visited[i] && dist[i] != INF && dist[i] < min){
+                min = dist[i];
+                u = i;
+            }
+        }
+
+        if(u == -1) break; // No reachable unvisited vertex left
+
+        visited[u] = 1;
+
+        for(w = 0; w < n; w++){
+            if(!visited[w] && cost[u][w] != INF){
+                if(dist[w] == INF || dist[u] + cost[u][w] < dist[w]){
+                    dist[w] = dist[u] + cost[u][w];
+                }
+            }
+        }
+
+        count++;
+    }
+
+    printf("\nShortest distances from vertex %d:\n", start + 1);
+    for(i = 0; i < n; i++){
+        if(dist[i] == INF)
+            printf("Vertex %d: Unreachable\n", i + 1);
+        else
+            printf("Vertex %d: %d\n", i + 1, dist[i]);
+    }
+}
+
+int main(){
+    int cost[MAX][MAX], n, source;
+
+    do{
+        printf("Enter the number of vertices (1 to %d): ", MAX);
+        scanf("%d", &n);
+    }while (n < 1 || n > MAX);
+
+    createmat(cost, n);
+
+    do{
+        printf("\nEnter the source vertex (1 to %d): ", n);
+        scanf("%d", &source);
+    }while (source < 1 || source > n);
+    dijkstra(cost, source - 1, n);
     return 0;
 }
